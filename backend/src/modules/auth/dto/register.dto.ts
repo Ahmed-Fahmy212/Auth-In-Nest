356003a156d09b583703d80d66cb30d836a2b24e
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-06 19:54:22 +0300

    feat:sparkles: added register dto .

---
+++ backend/src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterBodyDto {
+  @MinLength(4)
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @MinLength(6)
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
