8ed308e18bf793d1b31043827584aa26c00fbc69
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-06 19:56:52 +0300

    feat(Users):sparkles: added local strategy guard.

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,39 +1,55 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+
 
   public async findByUsernameOrFail(username: string): Promise<User> {
     try {
-      //!  try findOneOrFail here:
       return await this.userRepository.findOneBy({ username: username });
     } catch (error) {
       throw new NotFoundException(error.message);
     }
   }
+  ////////////////////////////////////////////////////////////////////
+  public async create(createUserDto: CreateUserDto): Promise<User> {
+    const { username, password } = createUserDto;
+    const result = await this.userRepository.createQueryBuilder()
+      .insert()
+      .into(User)
+      .values({ username, password })// password is hashed in the entity
+      .execute();
 
+    if (!result.raw || result.raw.length === 0) {
+      throw new HttpException('User isn`t created', 404);
+    }
+
+    const createdUser = await this.userRepository.findOne({ where: { id: result.raw[0].id } });
+
+    if (!createdUser) {
+      throw new NotFoundException('User not found after creation');
+    }
+    return createdUser;
+  }
+}
+////////////////////////////////////////////////////////////////////
   findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  return `This action returns all users`;
+}
+////////////////////////////////////////////////////////////////////
+findOne(id: number) {
+  return `This action returns a #${id} user`;
+}
+////////////////////////////////////////////////////////////////////
+update(id: number, updateUserDto: UpdateUserDto) {
+  return `This action updates a #${id} user`;
+}
+////////////////////////////////////////////////////////////////////
+remove(id: number) {
+  return `This action removes a #${id} user`;
 }
