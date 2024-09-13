f0472f5b2857eabcb313a8d194d71faecebf33cb
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-13 04:39:31 +0300

    refactor(main.ts): add exception filters and validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { EntityNotFoundExceptionFilter } from './utils/enitityEception';
+import { DuplicateExceptionFilter } from './utils/failedQuery';
+
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    // logger: ['error', 'warn', 'log'],
+    logger: ['error', 'warn', 'log'],
   });
-  
+
+  const configService = app.get(ConfigService);
   app.setGlobalPrefix('v1');
   app.use(helmet({ crossOriginResourcePolicy: false }));
-  
+
   app.enableCors({
     origin: '*',
     credentials: true,
-
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-  const configService = app.get(ConfigService);
+  app.useGlobalFilters(new DuplicateExceptionFilter());
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+  )
+
   const port = configService.get('APP_PORT');
 
   await app.listen(port);
