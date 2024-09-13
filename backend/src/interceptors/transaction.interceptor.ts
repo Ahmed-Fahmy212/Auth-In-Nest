b2b620638bce39df82145283c302d9ee72ff6292
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-13 04:40:36 +0300

    feat(Interceptors):sparkles: add transaction interceptor

---
+++ backend/src/interceptors/transaction.interceptor.ts
@@ -0,0 +1,45 @@
+import {
+    CallHandler,
+    ExecutionContext,
+    Injectable,
+    NestInterceptor,
+  } from '@nestjs/common';
+  import { Request } from 'express';
+  import { Observable, catchError, concatMap, finalize } from 'rxjs';
+  import { DataSource } from 'typeorm';
+  
+  export const Transaction = 'Transaction';
+  
+  @Injectable()
+  export class TransactionInterceptor implements NestInterceptor {
+    constructor(private dataSource: DataSource) {}
+  
+    async intercept(context: ExecutionContext, next: CallHandler<any>): Promise<Observable<any>> {
+      const req = context.switchToHttp().getRequest<Request>();
+  
+      const queryRunner = this.dataSource.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+  
+      console.log('💛 TransactionInterceptor started transaction'); 
+      req[Transaction] = queryRunner.manager;
+  
+      return next.handle().pipe(
+        concatMap(async (data) => {
+          await queryRunner.commitTransaction();
+          console.log('💛 TransactionInterceptor committed transaction');
+          return data;
+        }),
+        catchError(async (e) => {
+          await queryRunner.rollbackTransaction();
+          console.log('💛 TransactionInterceptor rolled back transaction');
+          throw e;
+        }),
+        finalize(async () => {
+          await queryRunner.release();
+          console.log('💛 TransactionInterceptor released connection');
+        }),
+      );
+    }
+  }
+  
