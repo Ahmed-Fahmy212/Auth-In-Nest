b21c555a4f01788fac56486f825b59da2969c4d5
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-26 23:24:31 +0300

    refactor(Navbar): Update height of navbar in layout component

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -2,7 +2,7 @@ import MobileMenu from "@/app/(dashboard)/_components/mobile-menu";
 
 const Navbar = () => {
   return (
-    <div className="p-4 border-b h-full flex items-center bg-white shadow-md">
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileMenu />
     </div>
   );
