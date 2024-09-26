7d525146381d918fb982a67e04d66d6efa639d79
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-26 23:20:44 +0300

    fix(Navbar): fix mobile menu component imports

--- frontend/app/(dashboard)/_components/mobile-menu.tsx
+++ frontend/app/(dashboard)/_components/mobile-menu.tsx
@@ -1,6 +1,6 @@
-import { SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Menu, Sheet, Sidebar } from "lucide-react";
-
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Sidebar from "./sidebar";
 const MobileMenu = () => {
   return (
     <Sheet>
@@ -8,7 +8,7 @@ const MobileMenu = () => {
         <Menu />
         {/* size={24} */}
       </SheetTrigger>
-      <SheetContent side={"left"} className="bg-white w-56 h-full">
+      <SheetContent side={"left"} className="bg-white p-0 h-full">
         <Sidebar />
       </SheetContent>
     </Sheet>
