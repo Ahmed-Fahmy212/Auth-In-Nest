0aa30d91f7a54d5ec3a6d1fe9adf04bc870f7268
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-26 23:21:13 +0300

    refactor(Sidebar): Update logo alignment and spacing

--- frontend/app/(dashboard)/_components/sidebar.tsx
+++ frontend/app/(dashboard)/_components/sidebar.tsx
@@ -3,8 +3,8 @@ import Logo from "./logo";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col overflow-auto shadow-md bg-white h-full w-full">
-      <div className="py-4 flex justify-center"> <Logo /></div>
+    <div className="flex flex-col overflow-auto shadow-md  bg-white h-full w-full">
+      <div className="py-6 flex justify-center "> <Logo /></div>
       <div className="flex flex-col "><SidebarRoutes/></div>
     </div>
   );
