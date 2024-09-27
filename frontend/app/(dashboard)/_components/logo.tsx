7daff031640afacd0a74b506b4ae54650ca17a01
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-27 17:59:22 +0300

    refactor(Logo): Update logo dimensions in logo component

--- frontend/app/(dashboard)/_components/logo.tsx
+++ frontend/app/(dashboard)/_components/logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 const Logo = () => {
-  return <Image height={36.81} width={150} alt="logo" src="/logo.svg" />;
+  return <Image height={40} width={163} alt="logo" src="/logo.svg" />;
 };
 
 export default Logo;
--- frontend/app/(dashboard)/_components/sidebar.tsx
+++ frontend/app/(dashboard)/_components/sidebar.tsx
@@ -4,7 +4,7 @@ import Logo from "./logo";
 const Sidebar = () => {
   return (
     <div className="flex flex-col overflow-auto shadow-md  bg-white h-full w-full">
-      <div className="py-6 flex justify-center "> <Logo /></div>
+      <div className="py-[20px] flex justify-center "> <Logo /></div>
       <div className="flex flex-col "><SidebarRoutes/></div>
     </div>
   );
