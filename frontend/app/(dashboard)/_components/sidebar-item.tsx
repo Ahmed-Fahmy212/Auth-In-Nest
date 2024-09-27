3fb76e2d977aba238da3bc1a28d9eef3445e0ca0
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-09-27 18:03:51 +0300

    front: side bar change color

--- frontend/app/(dashboard)/_components/sidebar-item.tsx
+++ frontend/app/(dashboard)/_components/sidebar-item.tsx
@@ -13,7 +13,7 @@ const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
   const isActive =
     (pathname === "/" && href === "/") ||
     pathname === href ||
-    (pathname?.startsWith(href) && href !== "/");
+    pathname?.startsWith(`${href}/`);
 
   const onClicky = () => {
     router.push(href);
@@ -24,13 +24,13 @@ const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       type="button"
       className={cn(
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500px] pl-6 transition-all duration-200 hover:text-slate-600 hover:bg-slate-300/20 ",
-        isActive && "text-sky-700 bg-sky-200/20 hover:text-sky-700"
+        isActive && "text-slate-800 bg-sky-800/10  hover:text-slate-800 hover:bg-sky-800/10 transition-all"
       )}
     >
       <div className="flex items-center gap-x-2 py-4 ">
         <Icon
           size={20}
-          className={cn("text-slate-500 ", isActive && "text-sky-700")}
+          className={cn("text-slate-500 ", isActive && "text-slate-800")}
         />
       </div>
 
@@ -38,8 +38,8 @@ const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
 
       <div
         className={cn(
-          "ml-auto opacity-0 border-2 bg-sky-700 h-full transition-all"
-          // isActive && "opacity-100"
+          "ml-auto opacity-0 border-2 h-full transition-all",
+          isActive && "opacity-1 border-sky-900"
         )}
       ></div>
     </button>
