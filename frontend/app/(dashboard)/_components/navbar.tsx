88cbe467adcc7a6f049889092535347decfcdf9c
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-03 18:01:08 +0300

    Refactor(dashboard): Add import for React in page component

---
+++ frontend/app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,20 @@
+import Logo from "@/app/(dashboard)/_components/logo";
+
+import MobileMenu from "@/app/(dashboard)/_components/mobile-menu";
+import { NavbarRoutes } from "@/components/navbar-routes";
+
+const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      {/* 1- add logo ----- 2- add items to login  */}
+      <div className="hidden md:flex">
+          <Logo />
+      </div>
+          <NavbarRoutes />
+
+      <MobileMenu />
+    </div>
+  );
+};
+
+export default Navbar;
