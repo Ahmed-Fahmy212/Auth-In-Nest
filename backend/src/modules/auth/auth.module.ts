7f154b4c89127f69ef8c8e5da496363b67a53310
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-03 18:04:51 +0300

    Refactor(auth): Update import path for LocalAuthGuard

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { LocalStrategy } from './strategies/local.strategy';
-import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { LocalAuthGuard } from '../../guards/local-auth.guard';
 import { CookieService } from 'src/utils/RefreshToken';
 import { EmailVerificationRepository } from './repositories/emailVerification.repository';
 import { Nodemailer } from '@crowdlinker/nestjs-mailer';
