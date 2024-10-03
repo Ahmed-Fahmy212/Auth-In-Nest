d9d4998b6934f86de2af69da98c002c7dec6f1a2
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-03 18:01:41 +0300

    Refactor(navbar): Remove deprecated navbar component and add new navbar routes component

---
+++ frontend/components/navbar-routes.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+import React from "react";
+
+// import { UserButton } from "@clerk/nextjs"
+
+export const NavbarRoutes = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isChapterPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isChapterPage ? ( //want here add logout
+        <button
+          onClick={() => router.push("/")}
+          type="button"
+          className="text-gray-700 hover:text-gray-900"
+        >
+          Log Out
+        </button>
+      ) : (
+        <>
+          <button
+            onClick={() => router.push("/teacher/courses")}
+            type="button"
+            className=" rounded-xl  text-white bg-[#042539] py-1 px-3 shadow-lg  "
+          >
+            Sign up
+          </button>
+          <button
+            onClick={() => router.push("/login")}
+            type="button"
+            className="text-gray-700 hover:text-black"
+          >
+            Login
+          </button>
+        </>
+      )}
+      {/* <a href="/" className="text-gray-700 hover:text-gray-900">Home</a> */}
+    </div>
+  );
+};
