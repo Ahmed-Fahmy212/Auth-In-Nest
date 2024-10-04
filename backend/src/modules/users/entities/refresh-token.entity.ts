b47e18796569643b245c3df4d41320e338c595fc
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-04 21:29:29 +0300

    refactor(Auth) : from revoking to rotation

--- backend/src/modules/users/entities/refresh-token.entity.ts
+++ backend/src/modules/users/entities/refresh-token.entity.ts
@@ -6,7 +6,7 @@ import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
 export class RefreshToken {
     @PrimaryGeneratedColumn("uuid")
     id: string;
-    
+
     @Column({ unique: true })
     token: string;
 
@@ -15,10 +15,4 @@ export class RefreshToken {
 
     @Column()
     expiration: Date;
-
-    @Column({ default: 1 }) // so revoke all versions in case logout or change password
-    tokenVersion: number;
-
-    @Column({ default: false })
-    isRevoked: boolean;
 }
