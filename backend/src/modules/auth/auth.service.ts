2552e59b3699a054414048f04f52e6db2eed5249
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-04 21:26:34 +0300

    refactor(Auth): Refactor AuthService and add refreshToken functionality

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -13,16 +13,19 @@ import { ForgottenPassword } from '../users/entities/forgotten-password.entity';
 import { RefreshToken } from '../users/entities/refresh-token.entity';
 import { RefreshTokenRepository } from './repositories/refreshToken.repository';
 import { ICreateRefreshTokenVerification } from './interfaces/create-refresh-token-verification.interface';
-
+import { isEmail } from 'class-validator';
+import { Response } from 'express';
+import { CookieService } from 'src/utils/RefreshToken';
 @Injectable()
 export class AuthService {
     constructor(
         @Inject(forwardRef(() => UsersService)) private usersService: UsersService,
         private readonly emailVerificationRepository: EmailVerificationRepository,
-        private readonly refreshTokenEntity: RefreshTokenRepository,
+        private readonly refreshTokenRepository: RefreshTokenRepository,
         private readonly jwtService: JwtService,
         // private nodeMailerService: Nodemailer<NodemailerDrivers.SMTP>,
         private readonly configService: ConfigService,
+        private readonly cookieService: CookieService,
     ) { }
     /////////////////////////////////////////////////////////////////////////////////
     public async validateUser(loginBodyDto: LoginBodyDto): Promise<User> {
@@ -52,24 +55,33 @@ export class AuthService {
     /////////////////////////////////////////////////////////////////////////////////
     // sign jwt access token
     public async signJwtAccessToken(user: Partial<User>): Promise<string> {
-        console.log('💛💛 user', user)
-        return await this.generateJwtToken(user,
+        const signedAccessToken = {
+            username: user.username,
+            email: user.email,
+        }
+        return await this.generateJwtToken(signedAccessToken,
             this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
             this.configService.get<string>('JWT_ACCESS_EXPIRATION_TIME'),
 
         );
     }
     public async signJwtRefreshToken(user: Partial<User>): Promise<string> {
+        const signedRefreshToken = {
+            username: user.username,
+            email: user.email,
+            // iat: new Date().getTime(),
+            // exp: new Date().setMinutes(new Date().getMinutes() + parseInt(this.configService.get<string>('JWT_REFRESH_EXPIRATION_TIME'))),
+        }
         return await this.generateJwtToken(
-            user,
+            signedRefreshToken,
             this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
             this.configService.get<string>('JWT_REFRESH_EXPIRATION_TIME'),
         );
     }
     /////////////////////////////////////////////////////////////////////////////////
-    private async generateJwtToken(user: Partial<User>, secret: string, expiresIn: string): Promise<any> {
+    private async generateJwtToken(data: any, secret: string, expiresIn: string): Promise<any> {
         try {
-            return await this.jwtService.signAsync({ ...user }, { secret, expiresIn })
+            return await this.jwtService.signAsync({ ...data }, { secret, expiresIn })
         } catch (error) {
             throw new ServiceUnavailableException(`Error generating token: ${error.message}`);
         }
@@ -104,10 +116,8 @@ export class AuthService {
             token,
             userId,
             expiration,
-            isRevoked: false,
-            tokenVersion: 1,
         }
-        await this.refreshTokenEntity.save(data);
+        await this.refreshTokenRepository.save(data);
     }
     ////////////////////////////////////////////////////////////////////
     public async verifyEmail(token: string): Promise<User> {
@@ -132,12 +142,37 @@ export class AuthService {
         return userData;
     }
     //////////////////////////////////////////////////////////////////
-    public async refreshToken(refreshToken: string) {
-        const payload = this.jwtService.verify(refreshToken, {
-            secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
-        });
-        const user = await this.usersService.findByUsername(payload.username);
-        return await this.signJwtAccessToken(user);
+    public async refreshToken(response: Response, refreshToken: string, user: Partial<User>) {
+        try {
+            const payload = await this.jwtService.verifyAsync(refreshToken, {
+                secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+            });
+            if (!payload) throw new UnauthorizedException('Invalid token');
+
+            const storedToken = await this.refreshTokenRepository.findOne(payload.userId);
+            if (!storedToken) {
+                throw new BadRequestException('Refresh token invalid ');
+            }
+            if (new Date() > storedToken.expiration) {
+                throw new BadRequestException('Refresh token is expired');
+            }
+            if (storedToken.userId !== user.id) {
+                throw new BadRequestException('Refresh token is invalid');
+            }
+            const newRefreshToken = await this.refreshTokenRepository.update(storedToken);
+            const newAccessToken = await this.signJwtAccessToken(user);
+            await this.cookieService.setRefreshTokenToHttpOnlyCookie(response, refreshToken);
+
+            response.send({
+                newRefreshToken,
+                newAccessToken,
+                expiresIn: new Date().setMinutes(new Date().getMinutes() + parseInt(this.configService.get<string>('JWT_ACCESS_EXPIRATION_TIME'))),
+            })
+        }
+        catch (error) {
+            console.log('error', error)
+            throw new UnauthorizedException('Can`t refresh token please login again');
+        }
     }
     //////////////////////////////////////////////////////////////////
     public async forgotPassword(email: string) {
