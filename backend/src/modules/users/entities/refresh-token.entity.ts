8432e8a3f9a15f594ceac55617813340348554f2
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-06 13:17:51 +0300

    Refactor(RefreshToken) :fire: used rotation instead revoke RefreshTokenRepository and RefreshToken entity

--- backend/src/modules/users/entities/refresh-token.entity.ts
+++ backend/src/modules/users/entities/refresh-token.entity.ts
@@ -1,18 +1,30 @@
-import { GeneralEntity } from 'shared/database/GeneralEntity';
-import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
+// import { GeneralEntity } from 'shared/database/GeneralEntity';
+// import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
 
-@Entity()
-@Index(['userId'])
-export class RefreshToken {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+// @Entity()
+// @Index(['userId'])
+// export class RefreshToken {
+//     @PrimaryGeneratedColumn("uuid")
+//     id: string;
 
-    @Column({ unique: true })
-    token: string;
+//     @Column({ unique: true })
+//     token: string;
 
-    @Column()
-    userId: string;
+//     @Column()
+//     ipAddress : string;
 
-    @Column()
-    expiration: Date;
-}
+//      @Column()
+//     userAgent : string;
+
+//     @Column({default: false})
+//     isRevoked: boolean;
+
+//     @Column()
+//     revokedAt: Date;
+
+//     @Column({default: 1})
+//     version: number;
+
+//     @Column()
+//     expiration: Date;
+// }
