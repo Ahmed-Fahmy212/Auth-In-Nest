d6e407668e2a35410c6e46f93939f1a5af52221e
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-14 22:56:17 +0300

    refactor(signIn): Update navbar routes and authentication handling

--- frontend/components/signsButtons.tsx
+++ frontend/components/signsButtons.tsx
@@ -4,27 +4,12 @@ import Link from "next/link";
 import React from "react";
 
 const SignInButton = () => {
-  const { data: session } = useSession();
-  console.log("💛💛 session data :", { session });
   ///TODO : change this
-  if (session && session.user.id && session.user.name)
-    return (
-      <div className="flex gap-4 ml-auto">
-        <p className="text-sky-600">{session.user.name}</p>
-        <Link
-          href={"/api/auth/signout"}
-          className="flex gap-4 ml-auto text-black"
-        >
-          Sign Out
-        </Link>
-      </div>
-    );
-
   return (
-    <div className="flex gap-4 ml-auto items-center">
+    <div className="flex gap-4  items-center">
       <Link
-        href={"/signup"}
-        className="flex gap-4 ml-auto bg-sky-800 text-white p-2 rounded hover:bg-sky-900 duration-300"
+        href={"api/auth/signup"}
+        className="flex gap-4  bg-sky-800 text-white p-2 rounded hover:bg-sky-900 duration-300"
       >
         Sign Up
       </Link>
@@ -32,7 +17,7 @@ const SignInButton = () => {
       
       <Link
         href={"/api/auth/signin"}
-        className="flex gap-4 ml-auto text-sky-800">
+        className="flex gap-4  text-sky-800">
         Sign In
       </Link>
     </div>
