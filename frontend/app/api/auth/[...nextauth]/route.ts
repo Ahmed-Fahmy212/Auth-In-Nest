b97877c06af1a47b479be9754d6069b7dd2021b5
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-14 22:54:33 +0300

    refactor(frontend): Update authentication route and session handling

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,30 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { Backend_URL } from "../../../../lib/Constants"; // Ensure this imports the correct URL
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 // const isDefaultSigninPage = req.method === "GET" && req.query.nextauth.includes("signin")
 // if (isDefaultSigninPage) providers.pop()
-
-const authOptions: NextAuthOptions = {
+// async function refreshToken(token: JWT): Promise<JWT> {
+//     const res = await fetch(Backend_URL + "auth/refresh-token", {
+//         method: "POST",
+//         credentials: "include",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token.tokens.refreshToken}`,
+//         },
+//     });
+//     console.log("refreshed");
+//     const response = await res.json();
+//     console.log("💛💛💛refreshed :", response, token);
+//     //TODO HAndle this 
+//     return {
+//         ...token,
+//         tokens: response,
+//     };
+// }
+export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
             name: 'Credentials',
@@ -17,54 +35,75 @@ const authOptions: NextAuthOptions = {
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials) {
-                if (!credentials?.username || !credentials?.password) return null;
+                if (!credentials?.username || !credentials?.password) {
+                    console.log("💛credentials ", null)
+                    return null;
+                }
 
                 const { username, password } = credentials;
-                
+
                 try {
-                    const res = await fetch(`${Backend_URL}/v1/auth/login`, {
+                    const res = await fetch(`http://localhost:8000/v1/auth/login`, {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({ username, password }),
                     });
 
-                    // Check if the response is ok
+
+                    console.log("💛💛💛Error res :", res.status, res.body);
                     if (!res.ok) {
                         const errorData = await res.json();
                         console.error(`Error: ${errorData.message}`);
-                        return null; // User authorization failed
+                        return null;
                     }
 
-                    const user = await res.json(); // Get the user object from response
-                    console.log("User authenticated:", user);
-                    return user; // Return the user object
+                    const user = await res.json();
+
+                    const extendedUser = {
+                        ...user.data,
+                        sub: user.data.id,
+                    };
+                    return extendedUser;
 
                 } catch (error) {
                     console.error('Fetch error:', error);
-                    return null; // Return null on error
+                    return null;
                 }
             }
         }),
     ],
     callbacks: {
-        async jwt({ token, user }) {
-            console.log("💛💛 token :", token);
-            console.log("💛💛 user :", user);
-            if (user) return { ...token, ...user };
+        async jwt({ token, user }: { token: JWT, user?: User }) {
+            if (user) {
+                return { ...token, ...user }
+            }
+            // if (new Date() < new Date(token?.tokens.expiresIn))
             return token;
+
+            // return await refreshToken(token);
         },
-        async session({ session, token }) {
-            console.log("💛💛 session :", session);
-            session.user = token.user || {};
-            session.tokens = token.tokens;
+        async session({ token,   }) {
+
+            session.user = {
+                id: token.sub as string,
+                name: token.name as string,
+                email: token.email as string,
+            };
+            session.accessToken = token.tokens.accessToken;
+            session.expires = new Date(token.tokens.expiresIn).toISOString();
+
+
+            console.log("💛💛 session:", session);
+            console.log("-----------------------------------------");
+
             return session;
         },
     }
     // , pages: {
     //     signIn: '/login',
     //     signOut: '/logout',
-        // error: '/login',
-        // verifyRequest: '/verify-request',
+    // error: '/login',
+    // verifyRequest: '/verify-request',
     //   },
 };
 const handler = NextAuth(authOptions);
