b6c83f339db7073c47b4a0cfc734b20867540da9
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-14 22:55:59 +0300

    refactor(frontend): Update navbar routes and authentication handling

--- frontend/components/navbar-routes.tsx
+++ frontend/components/navbar-routes.tsx
@@ -3,47 +3,86 @@
 import { useRouter, usePathname } from "next/navigation";
 import React from "react";
 import SignInButton from "./signsButtons";
-
-// import { UserButton } from "@clerk/nextjs"
+import { Button } from "./Button";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export const NavbarRoutes = () => {
   const router = useRouter();
   const pathname = usePathname();
-
-  const isTeacherPage = pathname?.startsWith("/teacher");
+  const session = useSession();
+  console.log("💛💛💛💛 session", session);
+  const isTeacherPage = pathname?.includes("/teacher");
   const isChapterPage = pathname?.includes("/chapter");
-
+  const isAuthenticated = session.data?.user?.name; //TODO add role
+  const isTeacher = session.data?.user?.role === "teacher";
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isChapterPage ? ( //want here add logout
-        <button
-          onClick={() => router.push("/")}
-          type="button"
-          className="text-gray-700 hover:text-gray-900"
-        >
-          Log Out
-        </button>
-      ) : (
+    <div className="flex gap-x-2">
+      {isAuthenticated ? (
         <>
-                  <SignInButton />
-
-          {/* <button
-            onClick={() => router.push("/teacher/courses")}
-            type="button"
-            className=" rounded-xl  text-white bg-[#042539] py-1 px-3 shadow-lg  "
-          >
-            Sign up
-          </button>
+          {isTeacher ? (
+            // teacher in main page or chapter page
+            <>
+              {isTeacherPage || isChapterPage ? (
+                <>
+                  <button
+                    onClick={() => signOut()}
+                    type="button"
+                    className="text-gray-700 hover:text-gray-900"
+                  >
+                    Log Out
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    onClick={() => router.push("/teacher")}
+                    className="bg-black hover:bg-black/90 text-white shadow shadow-gray-900 hover:shadow-gray-900"
+                  >
+                    courses Mode
+                  </Button>
+                  <button
+                    onClick={() => signOut()}
+                    type="button"
+                    className="text-gray-700 hover:text-gray-900"
+                  >
+                    Log Out
+                  </button>
+                </>
+              )}
+            </>
+          ) : (
+            // logged in student
+            <>
+              <button
+                onClick={() => signOut()}
+                type="button"
+                className="text-gray-700 hover:text-gray-900"
+              >
+                Log Out
+              </button>
+            </>
+          )}
+        </>
+      ) : (
+        // log in or sign up
+        <>
           <button
-            onClick={() => router.push("/login")}
+            onClick={() => signIn()}
             type="button"
             className="text-gray-700 hover:text-black"
           >
             Login
-          </button> */}
+          </button>
+          <button
+            onClick={() => router.push("api/auth/signup")}
+            type="button"
+            className="rounded-xl text-white bg-[#042539] py-2 px-3 shadow-lg hover:text-slate-300 hover:duration-300"
+          >
+            Sign up
+          </button>
         </>
       )}
-      {/* <a href="/" className="text-gray-700 hover:text-gray-900">Home</a> */}
     </div>
   );
 };
